7e9b87b7c1b66cc2cc1475398a5a45639facd232
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2021-12-14 19:48:24 -0300

    adicionando arquivos iniciais

---
+++ src/RegistroDeOrcamento.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class RegistroDeOrcamento
+{
+    public function registrar(Orcamento $orcamento): void
+    {
+        // chamar uma api de registro
+
+        // enviar os dados do orçamento
+    }
+}
