b9a69ed391e354a26920fdb57cf34b4b289fb897
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2021-12-16 20:05:19 -0300

    Implementando padrão Decorator.
    Nele é possível alterar agregar novos comportamentos a uma rotina em tempo de execução sem necessidade de alteração de código.
    Importante, apesar das alterações nas classes de imposto, os testes continuam funcionando.

--- testes.php
+++ testes.php
@@ -2,22 +2,23 @@
 
 use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\Impostos\Icms;
+use Alura\DesignPattern\Impostos\Ikcv;
 use Alura\DesignPattern\Impostos\Iss;
 use Alura\DesignPattern\Orcamento;
 use Alura\DesignPattern\CalculadoraDeImpostos;
 
 require "vendor/autoload.php";
 
-//$calculadora = new CalculadoraDeImpostos();
-//
-//$orcamento = new Orcamento();
-//$orcamento->valor = 200;
-//
-//echo $calculadora->calcula($orcamento, new Icms());
+$calculadora = new CalculadoraDeImpostos();
 
 $orcamento = new Orcamento();
-$orcamento->valor = 400;
-$orcamento->quantidadeItens = 4;
+$orcamento->valor = 100;
 
-$calculadora = new CalculadoraDeDescontos();
-echo $calculadora->calculaDescontos($orcamento);
+echo $calculadora->calcula($orcamento, new Icms(new Iss(new Ikcv())));
+
+//$orcamento = new Orcamento();
+//$orcamento->valor = 400;
+//$orcamento->quantidadeItens = 4;
+//
+//$calculadora = new CalculadoraDeDescontos();
+//echo $calculadora->calculaDescontos($orcamento);
